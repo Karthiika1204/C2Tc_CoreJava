df1bca983296d39f7f27cb774496b7146573a33d
USER <USER@DESKTOP-MCUNV7T>
2024-04-03 14:39:38 +0530

    First HelloWorld commit

---
+++ src/com/infosys/sampleproject1/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.infosys.sampleproject1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String a="Apple";
+	System.out.println(a.length())
+
+	}
+
+}
---
+++ src/com/infosys/sampleproject1/package-info.java
@@ -0,0 +1,1 @@
+package com.infosys.sampleproject1;
