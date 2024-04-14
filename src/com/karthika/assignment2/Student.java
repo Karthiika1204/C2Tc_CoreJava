53ba4f4a4fb32ebe85bddb9d0eb8c78c683bf277
USER <USER@DESKTOP-MCUNV7T>
2024-04-14 21:52:04 +0530

    Second commit

---
+++ src/com/karthika/assignment2/Student.java
@@ -0,0 +1,16 @@
+package com.karthika.assignment2;
+
+public class Student {
+	public void  Student(){
+		System.out.println("Student object is created");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Student s=new Student();
+	s.Student();
+		
+
+	}
+
+}
